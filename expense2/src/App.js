b67a7cf6c1a05d8367f1c1f4c36a050655f99ae6
ahaanbutt <60073494+ahaanbutt@users.noreply.github.com>
2020-08-01 21:59:40 +0500

    update

---
+++ expense2/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Header } from './Header';
+import { Balance } from './Balance';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
